6623e616bb4a37491941fa69c4f8b88ca972c970
H Stream <57220037+hstream-app@users.noreply.github.com>
2024-02-08 18:54:41 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,24 @@
+
+/*!
+ * H Stream App
+ * i.html
+ * 
+ * @license Copyright 2020, GreenSock. All rights reserved.
+ * Subject to the terms at https://greensock.com/standard-license or for Club GreenSock members, the agreement issued with that membership.
+ * @author: Hstream
+
+  <script type="text/javascript">
+    var alerted = localStorage.getItem('alerted') || '';
+    if (alerted != 'yes') {
+     alert("Using this Application \n \n" +
+   "You Agree to our terms and conditions  ");
+     localStorage.setItem('alerted','yes');
+    }
+</script>
+
+ */
+
+
+
+  <script> alert("Using this Application \n \n" +
+   "You Agree to our terms and conditions  ") </script>
