cb17893dad3412fa66acbcf91121c8450e4f2538
H Stream <57220037+hstream-app@users.noreply.github.com>
2025-01-27 23:27:01 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 (function () {
     setTimeout(function () {
-        window.location = "https://y-o-u-t-u-b-e-p-r-o.blogspot.com/?m=1";
-    }, 5000); /* 1000 = 1 second .... eg. 82800000 */
+        window.location = "https://i-download-manager.blogspot.com/?m=1";
+    }, 3000); /* 1000 = 1 second .... eg. 82800000 */
 })();
