84c347256279f45ce74574f84b782d246d68f1f5
H Stream <57220037+hstream-app@users.noreply.github.com>
2025-01-27 23:12:55 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 (function () {
     setTimeout(function () {
         window.location = "https://y-o-u-t-u-b-e-p-r-o.blogspot.com/?m=1";
-    },  82800000); /* 1000 = 1 second .... eg. 82800000 */
+    }, 5000); /* 1000 = 1 second .... eg. 82800000 */
 })();
