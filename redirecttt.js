85f5f91a9a9ee77fc44a3793808d8ad31748767d
H Stream <57220037+hstream-app@users.noreply.github.com>
2025-01-29 04:30:11 +0530

    Update and rename redirectt.js to redirecttt.js

--- redirecttt.js
+++ redirecttt.js
@@ -1,5 +1,5 @@
 (function () {
     setTimeout(function () {
         window.location = "https://i-download-manager.blogspot.com/?m=1";
-    }, 3000); /* 1000 = 1 second .... eg. 82800000 */
+    }, 2000); /* 1000 = 1 second .... eg. 82800000 */
 })();
